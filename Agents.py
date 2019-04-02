5c725c8ae56014e6803e3c2d68fe5c4c6415baa8
Boris <pakillaboris@gmail.com>
2019-04-02 12:08:58 +0200

    Added DYNA implementations

--- Agents.py
+++ Agents.py
@@ -6,7 +6,6 @@ from collections import defaultdict
 from functools import partial
 
 import numpy as np
-import maci.utils as utils
 from copy import deepcopy
 from abc import ABCMeta, abstractmethod
 import nashpy as nash
@@ -80,10 +79,15 @@ class RandomAgent(StationaryAgent):
 
 
 class QAgent:
-    def __init__(self, id_, action_num, n_states, env, alpha_decay_steps=10000., alpha=0.1, gamma=0.95, epsilon=0.5, verbose=True):
+    def __init__(self, id_, action_num, n_states, env, alpha_decay_steps=10000., alpha=0.1, gamma=0.95, epsilon=0.5, verbose=True, exp_type='e-greedy'):
         self.action_num = action_num
         self.id_ = id_
         self.Q = np.zeros((n_states, self.action_num))
+        self.startTau = self.tau = 1.0
+        self.endTau = 0.1
+        self.anneling_steps = 500
+        self.pre_trained = 100
+        self.exploration = exp_type
         # self.R = defaultdict(partial(np.zeros, self.action_num))
         self.count_R = defaultdict(partial(np.zeros, self.action_num))
         self.epsilon = epsilon
@@ -101,73 +105,140 @@ class QAgent:
             self.Q[s, a] = (1 - self.alpha) * self.Q[s, a] + self.alpha * r
         else:
             self.Q[s, a] = (1 - self.alpha) * self.Q[s, a] + self.alpha * (r + self.gamma * V - self.Q[s, a])
-        print(self.epoch)
-        self.update_policy(s, a, env)
+        # print(self.epoch)
         self.epoch += 1
 
     def val(self, s):
         return np.max(self.Q[s])
 
-    def update_policy(self, s, a, env):
-        Q = self.Q[s]
-        self.pi[s] = (Q == np.max(Q)).astype(np.double)
-
     def act(self, s, exploration):
-        if exploration and random.random() < self.epsilon:
-            return np.random.choice(np.arange(self.action_num), 1)[0]
-        else:
-            if self.verbose:
-                print('Agent {}--------------'.format(self.id_))
-                print('Q of agent {}: state {}: {}'.format(self.id_, s, str(self.Q[s])))
-                print('pi of agent {}: state {}: {}'.format(self.id_, s, self.pi[s]))
-                # print('payoff of agent {}: state {}: {}'.format(self.id_, s, self.R[s]))
-                print('Agent {}--------------'.format(self.id_))
-            return np.argmax(self.Q[s])
+
+        if self.exploration == 'e-greedy':
+            if random.random() < self.epsilon:
+                return np.random.choice(np.arange(self.action_num), 1)[0]
+            else:
+                if self.verbose:
+                    print('Agent {}--------------'.format(self.id_))
+                    print('Q of agent {}: state {}: {}'.format(self.id_, s, str(self.Q[s])))
+                    # print('payoff of agent {}: state {}: {}'.format(self.id_, s, self.R[s]))
+                    print('Agent {}--------------'.format(self.id_))
+                return np.argmax(self.Q[s])
+
+        elif self.exploration == 'boltzmann':
+            stepDrop = (self.startTau - self.endTau) / self.anneling_steps
+            if self.tau > self.endTau and self.epoch > self.pre_trained:
+                self.tau -= stepDrop
+            probs = np.exp(self.Q[s]/self.tau)/np.sum(np.exp(self.Q[s]/self.tau))
+            return np.random.choice(np.arange(self.action_num), p=probs)
 
 
 class NashAgent:
-    def __init__(self, id_, action_num, n_states, env, alpha_decay_steps=10000., alpha=0.1, gamma=0.95, epsilon=0.5, verbose=True):
+    def __init__(self, id_, action_num, n_states, env, alpha_decay_steps=10000., alpha=0.1, gamma=0.95, epsilon=0.5, verbose=True, dyna=False, planning_steps=1, exp_type='e-greedy'):
         self.action_num = action_num
         self.id_ = id_
         self.Q = np.zeros((2, n_states, self.action_num, self.action_num))
         self.Q_alone = np.zeros((n_states, self.action_num))
         self.epsilon = epsilon
+        self.startTau = self.tau = 1.0
+        self.endTau = 0.1
+        self.anneling_steps = 500
+        self.pre_trained = 1000
+        self.exploration = exp_type
+        self.dyna_update = False #let's add N planning updates. Let's call it Dyna-N
+        if dyna:
+            self.dyna_update = True
+            self.planning_steps = planning_steps
+            self.dyna_model = {}
+            self.rewards_dyna = {}
         self.gamma = gamma
         self.base_alpha = alpha
         self.epoch = 0
         self.pi = defaultdict(partial(np.random.dirichlet, [1.0] * self.action_num))
         self.verbose = verbose
-        self.history = np.zeros((n_states, self.action_num, self.action_num))
+        self.history_alpha = np.zeros((n_states, self.action_num, self.action_num))
+        self.history_act = np.zeros((n_states, 1))
         # self.pi_history = [deepcopy(self.pi)]
 
-    def update_nash(self, s, a, o, nash_actions, rewards, s2, env, done=False):
-        self.history[s, a, o] += 1
+    def update_nash(self, s, a, o, nash_actions, rewards, s2, done=False):
+
+        if self.dyna_update:
+            if s2 not in self.dyna_model:
+                self.dyna_model[s2] = [[s, a, o]]
+                self.rewards_dyna[s2] = [[rewards, done]]
+            else:
+                if [s, a, o] not in self.dyna_model[s2]:
+                    self.dyna_model[s2].append([s, a, o])
+                    self.rewards_dyna[s2].append([rewards, done])
+
+                # except ValueError:
+                #     print([s, a, o, rewards, done])
+                #     print('FCKK')
+                #     print(self.dyna_model[s2])
+                #     input()
+
+        self.history_alpha[s, a, o] += 1
         nash_actions_local = nash_actions[self.id_]
         V = [0, 0]
-        if nash_actions_local is not None:
+        if None not in nash_actions_local:
             V[0] = self.Q[0, s2, nash_actions_local[0], nash_actions_local[1]]
             V[1] = self.Q[1, s2, nash_actions_local[1], nash_actions_local[0]]
+            # for i in range(len(nash_actions_local)):
+            #     for j in range(len(nash_actions_local)):# it's probabilities now
+            #         V[0] += self.Q[0, s2, i, j]*nash_actions_local[0][i]*nash_actions_local[1][j]
+            # for i in range(len(nash_actions_local)):
+            #     for j in range(len(nash_actions_local)):# it's probabilities now
+            #         V[1] += self.Q[1, s2, j, i]*nash_actions_local[0][i]*nash_actions_local[1][j]
         else:
             V[0] = self.val(s2, agent_num=0)
             V[1] = self.val(s2, agent_num=1)
 
-        alpha = self.base_alpha / (self.history[s, a, o])
+        alpha = self.base_alpha / (self.history_alpha[s, a, o])
 
-        # alpha = self.base_alpha
-
-        # V_alone = self.val(s2)
-        # if done:
-        #     self.Q[s, a] = (1 - self.alpha) * self.Q[s, a] + self.alpha * r
-        # else:
-        #     self.Q[s, a] = (1 - self.alpha) * self.Q[s, a] + self.alpha * (r + self.gamma * V - self.Q[s, a])
         if done:
             # self.Q_alone[s, a] = (1 - self.alpha) * self.Q_alone[s, a] + self.alpha * r
             self.Q[0, s, a, o] = (1 - alpha)*self.Q[0, s, a, o] + alpha*rewards[self.id_]  # my own Q
             self.Q[1, s, o, a] = (1 - alpha)*self.Q[1, s, o, a] + alpha*rewards[1-self.id_]  # Q of an other one
         else:
+            # try:
             self.Q[0, s, a, o] = (1 - alpha)*self.Q[0, s, a, o] + alpha*(rewards[self.id_] + self.gamma * V[0])
             self.Q[1, s, o, a] = (1 - alpha)*self.Q[1, s, o, a] + alpha*(rewards[1-self.id_] + self.gamma * V[1])
-        print(self.epoch)
+            # except ValueError:
+            #     print(V)
+            #     print(nash_actions_local)
+            #     print(nash_actions)
+
+        # Dyna update d as addition symbol means Dyna
+        if self.dyna_update:
+            count_outer = 0
+            count_inner = 0
+            for state_key in self.dyna_model.keys():
+                if count_outer <= self.planning_steps:
+                    dyna_nash_actions = self.nash_act(state_key, self.id_)
+                    V_dyna = [0, 0]
+                    if None not in dyna_nash_actions:
+                        V_dyna[0] = self.Q[0, state_key, dyna_nash_actions[0], dyna_nash_actions[1]]
+                        V_dyna[1] = self.Q[1, state_key, dyna_nash_actions[1], dyna_nash_actions[0]]
+                    else:
+                        V_dyna[0] = self.val(state_key, agent_num=0)
+                        V_dyna[1] = self.val(state_key, agent_num=1)
+
+                    for num in range(len(self.dyna_model[state_key])):
+                        if count_inner <= self.planning_steps:
+                            d_s, d_a, d_o = self.dyna_model[state_key][num]
+                            dywards, d_done = self.rewards_dyna[state_key][num]
+                            if done:
+                                self.Q[0, d_s, d_a, d_o] = (1 - alpha)*self.Q[0, d_s, d_a, d_o] + alpha*dywards[self.id_]  # my own Q
+                                self.Q[1, d_s, d_o, d_a] = (1 - alpha)*self.Q[1, d_s, d_o, d_a] + alpha*dywards[1-self.id_]  # Q of an other one
+                            else:
+                                # TODO use matrix summation for speed-up the computations
+                                self.Q[0, d_s, d_a, d_o] = (1 - alpha)*self.Q[0, d_s, d_a, d_o] + alpha*(dywards[self.id_] + self.gamma * V_dyna[0])
+                                self.Q[1, d_s, d_o, d_a] = (1 - alpha)*self.Q[1, d_s, d_o, d_a] + alpha*(dywards[1-self.id_] + self.gamma * V_dyna[1])
+                        else: break
+                        count_inner += 1
+                    count_outer += 1
+                else: break
+
+        # print(self.epoch)
         self.epoch += 1
 
     def val(self, state, agent_num):
@@ -175,12 +246,6 @@ class NashAgent:
 
     def val_alone(self, s):
         return np.max(self.Q[s])
-
-    # def update_policy(self, s, a, game):
-    #     self.pi[s] = utils.softmax(np.sum(np.multiply(self.Q[s], self.opponent_best_pi[s]), 1))
-    #     self.pi_history.append(deepcopy(self.pi))
-    #     self.opponent_best_pi_history.append(deepcopy(self.opponent_best_pi))
-    #     print('opponent pi of {}: {}'.format(self.id_, self.opponent_best_pi))
 
     def nash_act(self, s, agent_id):
         A = self.Q[0, s]
@@ -190,24 +255,38 @@ class NashAgent:
         eqs = list(rps.support_enumeration())
         nash_length = len(eqs)
         try:
-            nash_choice = np.random.choice(np.arange(nash_length), 1)[0]
+            # nash_choice = np.random.choice(np.arange(nash_length), 1)[0]  #Random choice of Nash-equilibrium
+            nash_choice = 0
             eqs_0 = eqs[nash_choice][0].tolist()
             choices[0] = np.random.choice(np.arange(len(eqs_0)), p=eqs_0)
             eqs_1 = eqs[nash_choice][1].tolist()
             choices[1] = np.random.choice(np.arange(len(eqs_1)), p=eqs_1)
+            # return [eqs_0, eqs_1]
             return choices
-        except ValueError:
-            return None
+        except IndexError:
+            return [None, None]
 
     def act(self, s, exploration):
-        if exploration and random.random() < self.epsilon:
-            return np.random.choice(np.arange(self.action_num), 1)[0]
-        else:
-            choice = np.unravel_index(np.argmax(self.Q[0, s], axis=None), self.Q[0, s].shape)[0]
-            if self.verbose:
-                print('Agent {}--------------'.format(self.id_))
-                print('Q of agent {}: state {}: {}'.format(self.id_, s, str(self.Q[0, s])))
-                print('pi of agent {}: state {}: {}'.format(self.id_, s, self.pi[s]))
-                # print('payoff of agent {}: state {}: {}'.format(self.id_, s, self.R[s]))
-                print('Agent {}--------------'.format(self.id_))
-            return choice
+
+        # eps = 1/(1 + self.history_act[s])
+        # self.history_act[s] += 1
+
+        if self.exploration == 'e-greedy':
+            if random.random() < self.epsilon:
+                return np.random.choice(np.arange(self.action_num), 1)[0]
+            else:
+                choice = np.unravel_index(np.argmax(self.Q[0, s], axis=None), self.Q[0, s].shape)[0]
+                # A = self.Q[0, s]
+                # B = self.Q[1, s]
+                # rps = nash.Game(A, B)
+                # eqs = list(rps.support_enumeration())
+                # eqs_0 = eqs[0][0].tolist()
+                # choice = np.random.choice(np.arange(len(eqs_0)), p=eqs_0)
+                return choice
+        elif self.exploration == 'boltzmann':
+            stepDrop = (self.startTau - self.endTau) / self.anneling_steps
+            if self.tau > self.endTau and self.epoch > self.pre_trained:
+                self.tau -= stepDrop
+            Q_max = np.max(self.Q[0, s], axis=1)/100
+            probs = np.exp(Q_max/self.tau)/np.sum(np.exp(Q_max/self.tau))
+            return np.random.choice(range(self.action_num), p=probs)
